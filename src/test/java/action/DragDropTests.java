85d3af5773a365b61ec3d4b4ab0467d6ee682b40
Yuliya <9757975@gmail.com>
2021-02-04 18:03:57 +0300

    add draganddrop test

---
+++ src/test/java/action/DragDropTests.java
@@ -0,0 +1,25 @@
+package action;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDropTests {
+
+    @Test
+    @DisplayName("Drag&Drop with selenide actions")
+    void selenideActionsTest() {
+        //open page
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        //move A rectangle instead B
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(500,150).release().perform();
+
+        //verify rectangles switched places
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
