c671f49e9323ecdde9a68a5452b3289ddb628ca5
Yuliya <9757975@gmail.com>
2021-02-14 17:18:42 +0300

    add asserts

--- src/test/java/action/DragDropTests.java
+++ src/test/java/action/DragDropTests.java
@@ -14,9 +14,12 @@ public class DragDropTests {
         //open page
         open("https://the-internet.herokuapp.com/drag_and_drop");
 
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
         //move A rectangle instead B
-        $("#column-a").dragAndDropTo($("#column-b"));
-        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(650,150).release().perform();
+        //$("#column-a").dragAndDropTo($("#column-b"));
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(650,150).release().perform();
 
         //verify rectangles switched places
         $("#column-a").shouldHave(text("B"));
