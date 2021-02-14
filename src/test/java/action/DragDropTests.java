2265a538223be61055ff53ad6038d3e5fda41aef
Yuliya <9757975@gmail.com>
2021-02-14 18:00:53 +0300

    fix dragndrop

--- src/test/java/action/DragDropTests.java
+++ src/test/java/action/DragDropTests.java
@@ -2,6 +2,9 @@ package action;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Point;
+
+import java.awt.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -10,16 +13,19 @@ public class DragDropTests {
 
     @Test
     @DisplayName("Drag&Drop with selenide actions")
-    void selenideActionsTest() {
+    void selenideActionsTest() throws AWTException {
         //open page
         open("https://the-internet.herokuapp.com/drag_and_drop");
 
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
 
+        Point b=$("#column-b").getLocation();
+        new Robot().mouseMove(b.getX()+100,b.getY()+200);
+
         //move A rectangle instead B
-        //$("#column-a").dragAndDropTo($("#column-b"));
-        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(650,150).release().perform();
+        $("#column-a").dragAndDropTo($("#column-b"));
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(650,150).release().perform();
 
         //verify rectangles switched places
         $("#column-a").shouldHave(text("B"));
